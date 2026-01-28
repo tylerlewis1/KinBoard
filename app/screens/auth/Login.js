ae7d42d405414d2e8a597e9f569d316b9a131c6e
Tyler Lewis <tylerlewis@Tylers-MacBook-Air.local>
2026-01-28 13:45:11 -0600

    login page style

--- app/screens/auth/Login.js
+++ app/screens/auth/Login.js
@@ -1,11 +1,47 @@
-import { Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import globalstyle from "../../styles/auth/global";
 export default function Login(){
+    const nav = useNavigation();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const signin = async() => {
+        if(email == "" || password == ""){
+            alert("You must enter a user name and a password");
+            return;
+        }
+
+    }
     return(
         <SafeAreaView>
-            <Text style={globalstyle.header}>Kin Board</Text>
+            {/* <Text style={globalstyle.header}>Kin Board</Text> */}
+            <Image style={globalstyle.logo} source={require("../../../assets/images/logotb.png")}/>
+            <View>
+                <TextInput
+                    value={email}
+                    onChange={setEmail}
+                    placeholder="Email"
+                    style={globalstyle.txtinput}
+                />
+                <TextInput
+                    value={password}
+                    onChange={setPassword}
+                    placeholder="Password"
+                    style={globalstyle.txtinput}
+                    secureTextEntry={true}
+                    autoComplete="current-password"
+                />
+            </View>
+            <TouchableOpacity 
+            style={globalstyle.button}
+            onPress={signin}
+            ><Text style={globalstyle.buttontxt}>Sign in</Text></TouchableOpacity>
 
+            <TouchableOpacity
+                onPress={() => {nav.navigate("Signup")}}
+            ><Text style={globalstyle.buttontxt}>Don't have a account?</Text></TouchableOpacity>
         </SafeAreaView>
     )
 }
--- app/styles/auth/global.js
+++ app/styles/auth/global.js
@@ -5,5 +5,36 @@ export default StyleSheet.create({
         fontSize: "80",
         fontWeight: "bold",
         textAlign: "center",
+    },
+    logo: {
+        width: "80%",
+        margin: "auto",
+        height: "50%",
+    },
+    txtinput: {
+        width: "80%",
+        backgroundColor: "#c1c0c0",
+        display: "block",
+        margin: "auto",
+        padding: 20,
+        color: "black",
+        borderRadius: 20,
+        fontSize: 15,
+        marginBottom: 20
+    },
+    button: {
+        backgroundColor: "#2EC4B6",
+        width: "80%",
+        display: "block",
+        margin: "auto", 
+        padding: 20,
+        
+        borderRadius: 20
+    },
+    buttontxt: {
+        textAlign: "center",
+        fontWeight: "500",
+        fontSize: 20
     }
+    
 });
