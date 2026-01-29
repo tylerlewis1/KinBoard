f53316e6da921e7b108ad393ed043b004dec3f6f
Tyler Lewis <tylerlewis@Tylers-Air.lan>
2026-01-29 15:08:20 -0600

    join circle modal added

--- app/screens/main/dash.js
+++ app/screens/main/dash.js
@@ -3,7 +3,7 @@ import Entypo from '@expo/vector-icons/Entypo';
 import { useNavigation } from "expo-router";
 import { useContext, useState } from "react";
 import { Dimensions, Modal, Text, TouchableOpacity, View } from "react-native";
-import { Pressable, TextInput } from "react-native-gesture-handler";
+import { Pressable } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import style from "../../styles/main/dash";
 import HouseHolds from "./comps/households";
@@ -29,10 +29,12 @@ export default function Dash() {
                 <Pressable style={style.modal} onPress={() => setModalVisible(false)}>
                     </Pressable>
                     <View style={style.modalcontent} >
-                        <TouchableOpacity onPress={() => {setModalVisible(false)}} style={{padding: 10}}><Text>Back</Text></TouchableOpacity>
+                        <TouchableOpacity onPress={() => {setModalVisible(false)}} style={{padding: 10}}><Text style={{fontWeight: "bold", fontSize: hp(1.8), position: "absolute", right: wp(4), top: hp(2)}}>X</Text></TouchableOpacity>
                         <View style={style.form}>
-                            <Text style={style.title}>Create new circle</Text>
-                            <TextInput placeholder="Name" style={style.input}/>
+                            <Text style={style.title}>Add a new circle</Text>
+                            <TouchableOpacity 
+                                        style={style.button}
+                                        ><Text style={style.buttontxt}>Join Circle</Text></TouchableOpacity>
 
                             <TouchableOpacity 
                                         style={style.button}
